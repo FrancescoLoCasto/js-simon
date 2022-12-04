57deeca5b9e3b71bdada0cf451fc30d23903d4b9
MyUsername <my@email.com>
2022-12-04 14:10:33 +0100

    Create first function

--- Cronometer/js/script.js
+++ Cronometer/js/script.js
@@ -4,3 +4,13 @@ const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
 let counter = 0
+
+
+start.addEventListener('click', function (){
+
+   const timer = setInterval(() =>{
+
+   },100)
+
+
+})
